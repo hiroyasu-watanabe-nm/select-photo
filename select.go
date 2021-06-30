dd9a6b341f510997f161b2c32a7e20d2a1e0a537
nachipsyc <scarl.natsu729@gmail.com>
2021-06-30 15:39:51 +0900

    add comment

--- select.go
+++ select.go
@@ -12,13 +12,18 @@ import (
 
 func main() {
 	flag.Parse()
+
+	// 移動元ディレクトリを指定
 	source_dir := "../../../Desktop/" + flag.Arg(0)
+
+	// 移動先ディレクトリを指定
 	destination_dir := "../../../Desktop/" + flag.Arg(1)
 
 	var files []os.FileInfo = getFiles(source_dir)
 	moveRawFiles(files, getSelectedFileName(files), source_dir, destination_dir)
 }
 
+// 移動元ディレクトリの中にあるファイルを全て取得
 func getFiles(source_dir string) []os.FileInfo {
 	files, err := ioutil.ReadDir(source_dir)
 
@@ -29,6 +34,7 @@ func getFiles(source_dir string) []os.FileInfo {
 	return files
 }
 
+// 拡張子が".JPG"のファイル名を取り出し、拡張子部分を".ARW"に置き換えてファイル名をスライスに格納
 func getSelectedFileName(files []os.FileInfo) []string {
 	var selected_file_names []string
 	for _, file := range files {
@@ -40,6 +46,7 @@ func getSelectedFileName(files []os.FileInfo) []string {
 	return selected_file_names
 }
 
+// 移動元ディレクトリのファイルの中で、ファイル名が、スライス内の要素と一致するファイルを移動先ディレクトリに移動
 func moveRawFiles(files []os.FileInfo, selected_file_names []string, source_dir string, destination_dir string) {
 	for _, file := range files {
 		for _, selected_file_name := range selected_file_names {
